8a81becd7ccb94968203a27c69e2e89350c86bb0
Gustavo Oliveira <gustavo.macedo.ext@dasa.com.br>
2022-06-03 23:06:15 -0300

    feat: create domain layer

---
+++ src/domain/models/customer.ts
@@ -0,0 +1,27 @@
+interface ICompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface IAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+export interface Customer {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: IAddress;
+  phone: string;
+  website: string;
+  company: ICompany;
+}
